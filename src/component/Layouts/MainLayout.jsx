b1e877cbb7c47c35ee57f311c7ab5803c60d697b
Zykiria21 <l200200184@student.ums.ac.id>
2023-11-22 08:05:01 +0700

    repairing hero section

--- src/component/Layouts/MainLayout.jsx
+++ src/component/Layouts/MainLayout.jsx
@@ -1,8 +1,10 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
 
 export const MainLayout = ({ title, children }) => {
   return (
     <>
-      <div className="mx-auto max-w-7xl relative">{children}</div>
+      <div className='mx-auto max-w-7xl relative'>{children}</div>
     </>
   );
 };
