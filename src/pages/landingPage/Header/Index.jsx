9294f1bfe67d34f9336699df74b1ef44f1949caa
Zykiria21 <l200200184@student.ums.ac.id>
2023-11-22 10:55:21 +0700

    beautify header

--- src/pages/landingPage/Header/Index.jsx
+++ src/pages/landingPage/Header/Index.jsx
@@ -3,11 +3,11 @@ import Logo2 from '../../../assets/image/logo2.jpg';
 const Header = () => {
   return (
     <section id='Header'>
-      <div className='hero min-h-screen' style={{backgroundImage: 'url(https://daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.jpg)'}}>
-        <div className='hero-overlay bg-opacity-60'></div>
+      <div className='hero min-h-screen' style={{backgroundImage: `url(${Logo2})`}}>
         <div className='hero-content text-center text-neutral-content'>
           <div className='max-w-l'>
-            <h1 className='mb-5 text-5xl font-bold'>
+          <h1></h1>
+            <h1 className='mb-5 text-5xl font-bold text-white'>
               Hai, Kamu Mau Wisata Kemana
             </h1>
             <label
@@ -37,13 +37,13 @@ const Header = () => {
               <input
                 type='search'
                 id='default-search'
-                className='block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+                className='block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-xl bg-gray-20 focus:ring-black focus:border-black dark:bg-white dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
                 placeholder='Search Mockups, Logos...'
                 required
               />
               <button
                 type='submit'
-                className='text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'
+                className='text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700'
               >
                 Search
               </button>
