d6359e1488ab48432a4a095532d9122893699192
Ervanariyulianto <yuliantoervan70@gmail.com>
2023-11-24 18:38:55 +0700

    beautify section

--- src/pages/landingPage/SectionDestinasi/Index.jsx
+++ src/pages/landingPage/SectionDestinasi/Index.jsx
@@ -2,7 +2,7 @@ import Logo2 from '../../../assets/image/logo2.jpg';
 
 const SectionDestinasi = () => {
   return (
-    <section id='Destinasi' className='bg-white-200 h-80 '>
+    <section id='Destinasi' className='bg-white-200 h-80 my-5'>
       <div className='flex flex-wrap min-h-full items-center justify-center bg-amber-400'>
         <div className='mx-auto flex w-full text-center flex-col gap-6 my-5'>
           <h1 className='font-poppins text-2xl font-semibold sm-text-2xl text-white'>
@@ -16,97 +16,97 @@ const SectionDestinasi = () => {
             Indonesia.
           </h2>
         </div>
-        <div className='grid grid-cols-1 md:grid-cols-4 gap-3'>
-          <div className='group relative items-center justify-center overflow-hidden cursor-pointer rounded-2xl'>
-            <div className='h-70 w-72'>
+        <div className='grid grid-cols-1 md:grid-cols-4 gap-y-1.5'>
+          <div className='group relative items-center justify-center overflow-hidden cursor-pointer rounded-sm'>
+            <div className='h-50 w-52 px-2'>
               <img className='w-full object-cover' src={Logo2} alt='' />
             </div>
             <div className='absolute inset-0  bg-gradiant-to-b form-transparent via-transparent'>
-              <div className='absolute inset-0 flex flex-col items-center justify-center px-4 text-center translate-y-[100%] group-hover:translate-y-20 transition-all'>
+              <div className='absolute inset-0 flex flex-col items-center justify-center px- text-center translate-y-[100%] group-hover:translate-y-5 transition-all'>
                 <button className='rounded-full bg-orange-800 py-2 px-3.5 text-sm capitalize text-white justify-beetwen'>
                   See More
                 </button>
               </div>
             </div>
           </div>
-          <div className='group relative items-center justify-center overflow-hidden cursor-pointer rounded-2xl'>
-            <div className='h-70 w-72'>
+          <div className='group relative items-center justify-center overflow-hidden cursor-pointer rounded-sm'>
+            <div className='h-50 w-52 px-2 '>
               <img className='w-full object-cover' src={Logo2} alt='' />
             </div>
             <div className='absolute inset-0  bg-gradiant-to-b form-transparent via-transparent'>
-              <div className='absolute inset-0 flex flex-col items-center justify-center px-4 text-center translate-y-[100%] group-hover:translate-y-20 transition-all'>
+              <div className='absolute inset-0 flex flex-col items-center justify-center px-4 text-center translate-y-[100%] group-hover:translate-y-5 transition-all'>
                 <button className='rounded-full bg-orange-800 py-2 px-3.5 text-sm capitalize text-white justify-beetwen'>
                   See More
                 </button>
               </div>
             </div>
           </div>
-          <div className='group relative items-center justify-center overflow-hidden cursor-pointer rounded-2xl'>
-            <div className='h-70 w-72'>
+          <div className='group relative items-center justify-center overflow-hidden cursor-pointer rounded-sm'>
+            <div className='h-50 w-52 px-2'>
               <img className='w-full object-cover' src={Logo2} alt='' />
             </div>
             <div className='absolute inset-0  bg-gradiant-to-b form-transparent via-transparent'>
-              <div className='absolute inset-0 flex flex-col items-center justify-center px-4 text-center translate-y-[100%] group-hover:translate-y-20 transition-all'>
+              <div className='absolute inset-0 flex flex-col items-center justify-center px-4 text-center translate-y-[100%] group-hover:translate-y-5 transition-all'>
                 <button className='rounded-full bg-orange-800 py-2 px-3.5 text-sm capitalize text-white justify-beetwen'>
                   See More
                 </button>
               </div>
             </div>
           </div>
-          <div className='group relative items-center justify-center overflow-hidden cursor-pointer rounded-2xl'>
-            <div className='h-70 w-72'>
+          <div className='group relative items-center justify-center overflow-hidden cursor-pointer rounded-sm'>
+            <div className='h-50 w-52 px-2'>
               <img className='w-full object-cover' src={Logo2} alt='' />
             </div>
             <div className='absolute inset-0  bg-gradiant-to-b form-transparent via-transparent'>
-              <div className='absolute inset-0 flex flex-col items-center justify-center px-4 text-center translate-y-[100%] group-hover:translate-y-20 transition-all'>
+              <div className='absolute inset-0 flex flex-col items-center justify-center px-4 text-center translate-y-[100%] group-hover:translate-y-5 transition-all'>
                 <button className='rounded-full bg-orange-800 py-2 px-3.5 text-sm capitalize text-white justify-beetwen'>
                   See More
                 </button>
               </div>
             </div>
           </div>
-          <div className='group relative items-center justify-center overflow-hidden cursor-pointer rounded-2xl'>
-            <div className='h-70 w-72'>
+          <div className='group relative items-center justify-center overflow-hidden cursor-pointer rounded-sm'>
+            <div className='h-50 w-52 px-2'>
               <img className='w-full object-cover' src={Logo2} alt='' />
             </div>
             <div className='absolute inset-0  bg-gradiant-to-b form-transparent via-transparent'>
-              <div className='absolute inset-0 flex flex-col items-center justify-center px-4 text-center translate-y-[100%] group-hover:translate-y-20 transition-all'>
+              <div className='absolute inset-0 flex flex-col items-center justify-center px-4 text-center translate-y-[100%] group-hover:translate-y-5 transition-all'>
                 <button className='rounded-full bg-orange-800 py-2 px-3.5 text-sm capitalize text-white justify-beetwen'>
                   See More
                 </button>
               </div>
             </div>
           </div>
-          <div className='group relative items-center justify-center overflow-hidden cursor-pointer rounded-2xl'>
-            <div className='h-70 w-72'>
+          <div className='group relative items-center justify-center overflow-hidden cursor-pointer rounded-sm'>
+            <div className='h-50 w-52 px-2'>
               <img className='w-full object-cover' src={Logo2} alt='' />
             </div>
             <div className='absolute inset-0  bg-gradiant-to-b form-transparent via-transparent'>
-              <div className='absolute inset-0 flex flex-col items-center justify-center px-4 text-center translate-y-[100%] group-hover:translate-y-20 transition-all'>
+              <div className='absolute inset-0 flex flex-col items-center justify-center px-4 text-center translate-y-[100%] group-hover:translate-y-5 transition-all'>
                 <button className='rounded-full bg-orange-800 py-2 px-3.5 text-sm capitalize text-white justify-beetwen'>
                   See More
                 </button>
               </div>
             </div>
           </div>
-          <div className='group relative items-center justify-center overflow-hidden cursor-pointer rounded-2xl'>
-            <div className='h-70 w-72'>
+          <div className='group relative items-center justify-center overflow-hidden cursor-pointer rounded-sm'>
+            <div className='h-50 w-52 px-2'>
               <img className='w-full object-cover' src={Logo2} alt='' />
             </div>
             <div className='absolute inset-0  bg-gradiant-to-b form-transparent via-transparent'>
-              <div className='absolute inset-0 flex flex-col items-center justify-center px-4 text-center translate-y-[100%] group-hover:translate-y-20 transition-all'>
+              <div className='absolute inset-0 flex flex-col items-center justify-center px-4 text-center translate-y-[100%] group-hover:translate-y-5 transition-all'>
                 <button className='rounded-full bg-orange-800 py-2 px-3.5 text-sm capitalize text-white justify-beetwen'>
                   See More
                 </button>
               </div>
             </div>
           </div>
-          <div className='group relative items-center justify-center overflow-hidden cursor-pointer rounded-2xl'>
-            <div className='h-70 w-72'>
+          <div className='group relative items-center justify-center overflow-hidden cursor-pointer rounded-sm'>
+            <div className='h-50 w-52 px-2'>
               <img className='w-full object-cover' src={Logo2} alt='' />
             </div>
             <div className='absolute inset-0  bg-gradiant-to-b form-transparent via-transparent'>
-              <div className='absolute inset-0 flex flex-col items-center justify-center px-4 text-center translate-y-[100%] group-hover:translate-y-20 transition-all'>
+              <div className='absolute inset-0 flex flex-col items-center justify-center px-4 text-center translate-y-[100%] group-hover:translate-y-5 transition-all'>
                 <button className='rounded-full bg-orange-800 py-2 px-3.5 text-sm capitalize text-white justify-beetwen'>
                   See More
                 </button>
