ea9502a3a868c6a8a8290d616b9b832a7a3573e9
Zykiria21 <l200200184@student.ums.ac.id>
2023-11-28 19:22:18 +0700

    add sectionJelajah

--- src/pages/cityPage/Index.jsx
+++ src/pages/cityPage/Index.jsx
@@ -3,6 +3,7 @@ import Navbar from '../../component/Navbar';
 import Header from '../../component/Header/Index';
 import Footer from '../../component/Footer';
 import Logo3 from '../../assets/image/logo3.jpg'
+import SectionJelajahi from './SectionJelajahi/Index';
 
 
 
@@ -12,6 +13,7 @@ const CityPage = () => {
       <MainLayout>
         <Navbar />
         <Header  search={true} placeholder={'Cari Aktivitas Destinasi'} image={Logo3} title={'mulai petualangan berwisatamu dari sini'}/>
+        <SectionJelajahi />
         <Footer />
       </MainLayout>
     </>
--- src/pages/cityPage/SectionJelajahi/Index.jsx
+++ src/pages/cityPage/SectionJelajahi/Index.jsx
@@ -1,10 +1,23 @@
+import Logo2 from '../../../assets/image/logo2.jpg';
 
 const SectionJelajahi = () => {
   return (
-    <div>
-
+    <div className='bg-white'>
+      <div className='font-semibold text-center text-5xl pt-6 text-amber-400'>
+        <h1>Destinasi Tur Menarik</h1>
+      </div>
+      <div className='text-center font-small text-black pt-6 text-xl'>
+        <p>
+          Jelajahi Beragam Destinasi Wisata Populer Dengan Paket Tur Pilihan Ini
+        </p>
+      </div>
+      <div className='grid grid-cols-3 gap-8 py-6 mx-60'>
+        <img src={Logo2}></img>
+        <img src={Logo2}></img>
+        <img src={Logo2}></img>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default SectionJelajahi;
