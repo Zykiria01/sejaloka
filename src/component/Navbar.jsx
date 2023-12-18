3b9f39dd09837ba5c3c0df788b7e5fce66d803b9
Ervanariyulianto <yuliantoervan70@gmail.com>
2023-12-18 13:50:32 +0700

    revisi navbar

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -10,16 +10,16 @@ const Navbar = () => {
   const [isOpen, setisOpen] = useState(false);
   
   return (
-    <nav className={`flex flex-col md:flex-row items-start justify-between right-0 left-0 absolute md:items-center p-6 bg-opacity-50 bg-slate-100 h-auto gap-7`}>
+    <nav className={`flex flex-col md:flex-row items-start justify-between right-0 left-0 absolute md:items-center p-6 bg-opacity-50 bg-slate-100 h-auto gap-7 md:gap-2`}>
       <div className='flex justify-between w-full md:w-max gap-3 items-center font-poppins font-bold'>
-        <img className='w-40 h-7 md:w-40 md:h-10 items-center' src={Logo1} alt='' />
-        <div className="md:hidden">
+        <img className='w-40 h-7 md:w-42 md:h-10 items-center' src={Logo1} alt='' />
+        <div className="md:hidden ">
           <TiThMenu onClick={() => setisOpen(!isOpen)}/>
         </div>
       </div>
 
       <div className={`${isOpen ? 'flex ':'hidden'} md:flex`}>
-        <ul className='flex flex-col md:flex-row gap-3 md:gap-10 text-lg'>
+        <ul className='flex flex-col md:flex-row gap-3 md:gap-7 text-lg'>
           <NavButton href='/'>Beranda</NavButton>
           <NavButton href='#Pengalaman'>Pengalaman</NavButton>
           <NavButton href='#Destinasi'>Destinasi</NavButton>
@@ -28,7 +28,7 @@ const Navbar = () => {
       <div className={`${isOpen ? 'flex z-10':'hidden'} md:flex`}>
       {authService.isAuthorized() ? (
         <Link to='/profile/account'>
-          <SignButton title={'Akun Saya'} />
+          <SignButton title={'Akun'} />
         </Link>
       ) : (
         <Link to='/Login'>
