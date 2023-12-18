c5ce61bc0c22bf1795a36db8f9981b20e9bcdf86
Ervanariyulianto <yuliantoervan70@gmail.com>
2023-12-18 10:49:14 +0700

    setting new firebase

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -6,24 +6,29 @@ import { authService } from '../configs/auth/auth';
 
 const Navbar = () => {
   return (
-    <nav className='flex justify-between right-0 left-0 absolute items-center p-6 bg-opacity-50 bg-slate-100'>
-      <div className='flex gap-3 items-center font-poppins font-bold'>
-        <img className='w-30 h-10 items-center' src={Logo1} alt='' />
+    <nav className='flex justify-between right-0 left-0 absolute items-center p-6 bg-opacity-50 bg-slate-100 h-20 '>
+      <div className='gap-3 items-center font-poppins font-bold'>
+        <img className='w-20 h-7 md:w-30 md:h-10 items-center' src={Logo1} alt='' />
       </div>
-      <ul className='flex gap-10 text-lg'>
-        <NavButton href='/landingPage'>Beranda</NavButton>
-        <NavButton href='#Pengalaman'>Pengalaman</NavButton>
-        <NavButton href='#Destinasi'>Destinasi</NavButton>
-      </ul>
-      {authService.isAuthorized() ? (
-        <Link to='/profile/account'>
-          <SignButton title={'Akun Saya'} />
-        </Link>
-      ) : (
-        <Link to='/Login'>
-          <SignButton title={'Sign In'} />
-        </Link>
-      )}
+      <div className=''>
+        <div className='flex'>
+          <ul className='flex gap-10 text-lg'>
+            <NavButton href='/landingPage'>Beranda</NavButton>
+            <NavButton href='#Pengalaman'>Pengalaman</NavButton>
+            <NavButton href='#Destinasi'>Destinasi</NavButton>
+        </ul>
+        {authService.isAuthorized() ? (
+          <Link to='/profile/account'>
+            <SignButton title={'Akun Saya'} />
+          </Link>
+        ) : (
+          <Link to='/Login'>
+            <SignButton title={'Sign In'} />
+          </Link>
+        )}
+        </div>
+      </div>
+      
     </nav>
   );
 };
